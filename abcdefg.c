1e2c40151ce6f588ca122ff2f14deff698bf788f
wwweert123 <puachengxuan.fp@gmail.com>
2023-10-11 22:30:01 +0800

    initial commit

---
+++ abcdefg.c
@@ -0,0 +1,17 @@
+/**
+ * CS1010 Semester 1 AY23/24
+ * Bonus question By Frederick
+ *
+ * @file: board.c
+ * @author: XXX (Group YYY)
+ */
+
+#include "cs1010.h"
+
+int main()
+{
+    // TODO: Find the distance between the points p and q
+    char p, q;
+    scanf(" %c",&p);
+    scanf(" %c",&q);
+}
